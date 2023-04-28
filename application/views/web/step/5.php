9da7bbdfd8f484ce3057f1cdf794200446c6ee56
Muhammad Rifky Wardana <rwardana998@gmail.com>
2023-04-28 11:01:19 +0800

    Upload Project

---
+++ application/views/web/step/5.php
@@ -0,0 +1,43 @@
+<div class="panel panel-primary">
+  <!-- <div class="panel-heading">
+    <h2><strong class="text-success" style="color:#eee;">Rayonisasi</strong></h2>
+  </div> -->
+  <div class="panel-body">
+
+  <div class="form-group">
+      <label class="col-sm-3 control-label" style="text-align:right; margin-top:6px">Jenis Pendaftaran SMK Negeri 1 Banjarbaru<span class="text-danger">*</span></label>
+      <div class="col-sm-9" style="margin-top:3px;">
+        <select class="form-control bg-blue class" data-placeholder="Jenis Pendaftaran SMK Negeri 1 Banjarbaru" name="rayonisasi" data-parsley-group="block4" data-parsley-errors-container='div[id="error-rayonisasi"]' required>
+                <option value=""> - Pilih Salah Satu - </option>
+                <option value="Umum">Kategori Umum</option>
+                <option value="Zonasi">Kategori Zonasi</option>
+        </select>
+        <div id="error-rayonisasi" style=" background:#FFBABA;color: #D8000C; width:auto;border-radius:5px;padding-left:10px;"></div>
+      </div>
+  </div>
+
+  </div>
+</div>
+
+
+<div class="col-md-12">
+  <hr>
+  <blockquote>
+    <p><b>CATATAN:</b>
+        <ol>
+          <li>Field isian dengan tanda <span class="text-danger ">*</span><b class="text-danger">wajib diisi</b>.</li>
+          <li>
+            <table width="100%" border="0">
+              <tr>
+                <th colspan="2">Untuk kategori Zonasi pada saat verifikasi membawa berkas pendukung sbb :</th>
+              </tr>
+              <tr>
+                <td valign="top"><i class="fa fa-check"></i></td>
+                <td><i>Membawa surat keterangan domisili (fotocopy kartu keluarga)</i></td>
+              </tr>
+            </table>
+          </li>
+        </ol>
+    </p>
+  </blockquote>
+<div>
